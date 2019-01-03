a13fe1b4028cdf89936e777380999ca1bf347869
Jordon Davidson <jodavidson@mta.ca>
2019-01-03 15:15:16 -0400

    fix location of sass files

--- Gruntfile.js
+++ Gruntfile.js
@@ -39,7 +39,7 @@ module.exports = function(grunt) {
           },
         watch: {
             sass: {
-                files: 'src/**/*.sass',
+                files: 'src/**/*.scss',
                 tasks: ['sass']
             },
             css: {
